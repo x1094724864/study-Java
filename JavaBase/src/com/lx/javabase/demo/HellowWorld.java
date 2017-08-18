997a49b898b3a76d8d402446ab86b5b0f354395b
Li Xiang <1094724864@qq.com>
2017-08-18 10:52:18 +0800

    diyicixuexi Signed-off-by: Li Xiang <1094724864@qq.com>

---
+++ JavaBase/src/com/lx/javabase/demo/HellowWorld.java
@@ -0,0 +1,20 @@
+package com.lx.javabase.demo;
+
+/**
+ * @author Administrator
+ *
+ */
+/**
+ * @author Administrator
+ *
+ */
+public class HellowWorld {
+
+	public static void main(String[] args) {
+//ctrl+d 可以删除光标所在行
+		System.out.println("hellow world");
+		
+		
+	}
+
+}
---
+++ JavaBase/src/com/lx/javabase/demo/newP.java
@@ -0,0 +1,13 @@
+package com.lx.javabase.demo;
+
+public class newP {
+
+	public static void main(String[] args) {
+    System.out.println("你的姓名是："+args[0]);
+		
+	System.out.println("你今年"+args[1]);
+	
+	
+	}
+
+}
