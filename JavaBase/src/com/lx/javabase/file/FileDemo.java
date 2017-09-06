6f75dc3b6f4a16b3d997a2f087378037e85ca806
x1094724864 <1094724864@qq.com>
2017-09-06 11:22:50 +0800

    9/6

---
+++ JavaBase/src/com/lx/javabase/file/FileDemo.java
@@ -0,0 +1,18 @@
+package com.lx.javabase.file;
+
+import java.io.File;
+
+public class FileDemo {
+
+	public static void main(String[] args) {
+
+		File file1 = new File("E:\\JavaDoc\\GitHub\\study-Java\\JavaBase\\jbit.log");
+		File file2 = new File("E:\\JavaDoc\\GitHub\\study-Java");
+		System.out.println("file1.exists():" + file1.exists());
+		System.out.println("file1.getAbsolutePath():" + file1.getAbsolutePath());
+		System.out.println("file1.getAbsoluteFile():" + file1.getAbsoluteFile());
+		System.out.println("file1.canExecute():" + file1.canExecute());
+		System.out.println("file2.list():" + file2.exists());
+	}
+
+}
